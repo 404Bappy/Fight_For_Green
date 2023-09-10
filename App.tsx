5dd7cea9046516e059d20091bf7d28a4622d4b5e
Bappy71 <19103076@iubat.edu>
2023-09-10 16:37:20 +0600

    first commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import {View, Text, StatusBar} from 'react-native';
+const Stack = createStackNavigator();
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Home from './src/View/Screens/Home';
+import Details from './src/View/Screens/Details';
+import COLORS from './src/Consts/colors';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="dark-content" backgroundColor={COLORS.white} />
+      <Stack.Navigator screenOptions={{header: () => null}}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
