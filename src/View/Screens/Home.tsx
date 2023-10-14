5219ef8bbb64cadab4d4b6bea971bae4c942bc7e
Bappy71 <19103076@iubat.edu>
2023-10-14 17:24:40 +0600

    first commit

--- src/View/Screens/Home.tsx
+++ src/View/Screens/Home.tsx
@@ -1,12 +1,16 @@
-import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
 import React from 'react';
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+
 import COLORS from '../../Consts/colors';
 
 const Home = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
-        
+        <View>
+          <Text style={styles.headerTxt1}>Welcome to</Text>
+          <Text style={styles.headerTxt2}>Plants World</Text>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -24,5 +28,15 @@ const styles = StyleSheet.create({
     marginTop: 20,
     flexDirection: 'row',
     justifyContent: 'space-between',
+  },
+  headerTxt1: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: COLORS.dark,
+  },
+  headerTxt2: {
+    fontSize: 38,
+    fontWeight: 'bold',
+    color: COLORS.green,
   },
 });
