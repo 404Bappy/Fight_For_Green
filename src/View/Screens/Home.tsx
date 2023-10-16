9800780aa4f238e1bd9081032efc388f9b8d4ffb
Bappy71 <19103076@iubat.edu>
2023-10-16 11:50:43 +0600

    first commit

--- src/View/Screens/Home.tsx
+++ src/View/Screens/Home.tsx
@@ -6,14 +6,86 @@ import {
   StyleSheet,
   Image,
   TextInput,
+  FlatList,
 } from 'react-native';
 import COLORS from '../../Consts/colors';
 import {TouchableOpacity} from 'react-native';
+import Plant from '../../Consts/Plant';
+import { NavigationContainer } from '@react-navigation/native';
 
 const Home = () => {
   const [catergoryIndex, setCategoryIndex] = React.useState(0);
 
   const categories = ['POPULAR', 'ORGANIC', 'INDOORS', 'SYNTHETIC'];
+
+  const Card = ({Plant}}) => {
+    return (
+      <TouchableOpacity
+        activeOpacity={0.8}
+        onPress={() => Navigatio.navigate('Details', Plant)}>
+        <View style={styles.card}>
+          <View style={{alignItems: 'flex-end'}}>
+            <View
+              style={{
+                width: 30,
+                height: 30,
+                borderRadius: 20,
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: plant.like
+                  ? 'rgba(245, 42, 42,0.2)'
+                  : 'rgba(0,0,0,0.2) ',
+              }}>
+              {/* <Icon
+                name="favorite"
+                size={18}
+                color={plant.like ? COLORS.red : COLORS.black}
+              /> */}
+            </View>
+          </View>
+
+          <View
+            style={{
+              height: 100,
+              alignItems: 'center',
+            }}>
+            <Image
+              source={Plant.img}
+              style={{flex: 1, resizeMode: 'contain'}}
+            />
+          </View>
+
+          <Text style={{fontWeight: 'bold', fontSize: 17, marginTop: 10}}>
+            {Plant.name}
+          </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              marginTop: 5,
+            }}>
+            <Text style={{fontSize: 19, fontWeight: 'bold'}}>
+              ${Plant.price}
+            </Text>
+            <View
+              style={{
+                height: 25,
+                width: 25,
+                backgroundColor: COLORS.green,
+                borderRadius: 5,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <Text
+                style={{fontSize: 22, color: COLORS.white, fontWeight: 'bold'}}>
+                +
+              </Text>
+            </View>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -65,6 +137,21 @@ const Home = () => {
             </TouchableOpacity>
           ))}
         </View>
+      </View>
+      <View>
+        <FlatList
+          columnWrapperStyle={{justifyContent: 'space-between'}}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            marginTop: 10,
+            paddingBottom: 50,
+          }}
+          numColumns={2}
+          data={Plant}
+          renderItem={({item}) => {
+            return <Card Plant={item} />;
+          }}
+        />
       </View>
     </SafeAreaView>
   );
