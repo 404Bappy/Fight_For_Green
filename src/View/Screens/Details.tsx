ee9af6a99e76748d3933eccceba8ed6f8cee2097
Bappy71 <19103076@iubat.edu>
2023-10-18 10:12:17 +0600

    first commit

--- src/View/Screens/Details.tsx
+++ src/View/Screens/Details.tsx
@@ -1,12 +1,9 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
-const Details = () => {
-  return (
-    <View>
-      <Text>Details</Text>
-    </View>
-  );
+const Details = (navigation, Route) => {
+  return <SafeAreaView></SafeAreaView>;
 };
 
 export default Details;
