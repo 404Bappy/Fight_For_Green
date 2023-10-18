3b4c478be6ce57fb1b1064d516046c206c3bab60
Bappy71 <19103076@iubat.edu>
2023-10-18 11:48:26 +0600

    first commit

--- src/View/Screens/Details.tsx
+++ src/View/Screens/Details.tsx
@@ -1,11 +1,40 @@
-import {View, Text} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import COLORS from '../../Consts/colors';
+import {Image} from 'react-native';
+import {TouchableOpacity} from 'react-native';
 
-const Details = (navigation, Plant) => {
-  return <SafeAreaView>
-    
-  </SafeAreaView>;
+const Details = ({navigation, route}) => {
+  const plant = route.params;
+  return (
+    <SafeAreaView style={styles.safeViewStyl}>
+      <View style={styles.hdr}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Image
+            source={require('../../Assets/previous.png')}
+            style={styles.BrksImg}
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default Details;
+
+const styles = StyleSheet.create({
+  hdr: {
+    paddingHorizontal: 20,
+    marginTop: 20,
+    flexDirection: 'row',
+  },
+  safeViewStyl: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+  },
+  BrksImg: {
+    height: 28,
+    width: 28,
+  },
+});
